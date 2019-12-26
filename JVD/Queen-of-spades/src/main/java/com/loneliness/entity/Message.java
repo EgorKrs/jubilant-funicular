43078bb6a305d55307197131d3219353919d3718
EgorKrs <47493003+egorkrs@users.noreply.github.com>
2019-12-26 17:11:05 +0300

    finish MessageDAO

---
+++ JVD/Queen-of-spades/src/main/java/com/loneliness/entity/Message.java
@@ -0,0 +1,134 @@
+package com.loneliness.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Message {
+    private final int id;
+    private final int toUser;
+    private final int fromUser;
+    private final String message;
+    private final LocalDate date;
+
+    private Message(Builder builder) {
+        this.id = builder.id;
+        this.toUser = builder.toUser;
+        this.fromUser = builder.fromUser;
+        this.message = builder.message;
+        this.date = builder.date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getToUser() {
+        return toUser;
+    }
+
+    public int getFromUser() {
+        return fromUser;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message1 = (Message) o;
+        return id == message1.id &&
+                toUser == message1.toUser &&
+                fromUser == message1.fromUser &&
+                message.equals(message1.message) &&
+                date.equals(message1.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, toUser, fromUser, message, date);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", toUser=" + toUser +
+                ", fromUser=" + fromUser +
+                ", message='" + message + '\'' +
+                ", date=" + date +
+                '}';
+    }
+
+    public static class Builder{
+        private int id=0;
+        private int toUser=0;
+        private int fromUser=0;
+        private String message="";
+        private LocalDate date=LocalDate.now();
+
+        public Builder(){}
+
+        public Builder(Message otherMessage) {
+            this.id = otherMessage.id;
+            this.toUser = otherMessage.toUser;
+            this.fromUser = otherMessage.fromUser;
+            this.message = otherMessage.message;
+            this.date = otherMessage.date;
+        }
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setToUser(int toUser) {
+            this.toUser = toUser;
+            return this;
+        }
+
+        public Builder setFromUser(int fromUser) {
+            this.fromUser = fromUser;
+            return this;
+        }
+
+        public Builder setMessage(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder setDate(LocalDate date) {
+            this.date = date;
+            return this;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public int getToUser() {
+            return toUser;
+        }
+
+        public int getFromUser() {
+            return fromUser;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public LocalDate getDate() {
+            return date;
+        }
+        public Message build(){
+            return new Message(this);
+        }
+    }
+}
---
+++ JVD/Queen-of-spades/src/test/java/MessageDAOTest.java
@@ -0,0 +1,73 @@
+import com.github.javafaker.Faker;
+import com.loneliness.dao.sql_dao_impl.AccountDAO;
+import com.loneliness.dao.sql_dao_impl.MessageDAO;
+import com.loneliness.dao.sql_dao_impl.UserDAO;
+import com.loneliness.entity.Account;
+import com.loneliness.entity.Message;
+import com.loneliness.entity.User;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.beans.PropertyVetoException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Locale;
+
+public class MessageDAOTest {
+    private static Faker faker;
+    private MessageDAO dao=new MessageDAO();
+
+    public MessageDAOTest() throws PropertyVetoException {
+    }
+
+    @BeforeClass
+    public static void initialize()  {
+        faker=new Faker(new Locale("ru"));
+    }
+    private Collection<User> getUsers() throws PropertyVetoException {
+        UserDAO userDAO=new UserDAO();
+        return userDAO.receiveAll(new int[]{0,2});
+    }
+    private Message createValidMessage() throws PropertyVetoException {
+        Message.Builder builder = new Message.Builder();
+        Iterator<User> iterator= getUsers().iterator();
+        builder.setToUser(iterator.next().getId())
+                .setDate(LocalDate.now())
+                .setFromUser(iterator.next().getId())
+                .setMessage(faker.hipster().word());
+        return builder.build();
+    }
+    @Test
+    public void createTest() throws PropertyVetoException {
+        Message message=createValidMessage();
+        Message.Builder builder=new Message.Builder(message);
+        builder.setId(dao.create(message));
+        message=builder.build();
+        Assert.assertEquals(message,dao.receive(message));
+    }
+    @Test
+    public void updateTest(){
+        Message message=dao.receiveAll(new int[]{0,1}).iterator().next();
+        Message.Builder builder=new Message.Builder(message);
+        builder.setMessage(faker.crypto().md5());
+        Message changedMessage=builder.build();
+        dao.update(changedMessage);
+        Assert.assertNotEquals(message,dao.receive(changedMessage));
+    }
+    @Test
+    public void deleteTest(){
+        Message message=dao.receiveAll(new int[]{0,1}).iterator().next();
+        dao.delete(message);
+        Assert.assertNotEquals(message,dao.receive(message));
+    }
+    @Test
+    public void receiveTest(){
+        int lengthAll=dao.receiveAll().size();
+        int lengthInLim=dao.receiveAll(new int[]{0,lengthAll}).size();
+        Assert.assertEquals(lengthAll,lengthInLim);
+    }
+}
